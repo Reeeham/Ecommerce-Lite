b16fb081c5f70b276495c78b22d94632cdd7091b
@reeeham <reeehamgamal@gmail.com>
2021-05-29 20:40:49 +0200

    project initial setup

---
+++ src/components/Layout.js
@@ -0,0 +1,19 @@
+
+
+
+import React from 'react'
+import Content from './Content'
+import Footer from './Footer'
+import Header from './Header'
+
+function Layout() {
+    return (
+        <div>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+
+
+
+export const routes = [
+     {path: '/products',exact : true, name: 'Products', component :'Products'},
+     {path: '/about',exact : true, name: 'About', component :'About'},
+     {path: '/',exact : true, name: 'Home', component :'Home'}
+    ]
