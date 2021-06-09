1ec6c25fc074e4c98d499982f701bb1c061010cc
@reeeham <reeehamgamal@gmail.com>
2021-06-09 06:13:40 +0200

    slider

--- src/containers/Home/components/category-list/index.js
+++ src/containers/Home/components/category-list/index.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import "./index.scss"
+import Carousel from 'react-elastic-carousel';
+
+const breakPoints = [
+    { width: 1, itemsToShow: 3 }
+  ];
+  
 
 function CategoryList() {
     return (
-        <div className="container category-list">
+        <div className="container">
+        <Carousel breakPoints={breakPoints} className="category-list">
+ 
             <div className="category-card">
+                <div>
+                    <h1 className="category-title">Fashion</h1>
+                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
+                    <button className="category-btn">Shop Now</button>
+                </div>
+                <div className="bg-img">
+                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
+                </div>
+            </div>
+            <div className="category-card">
+                <div>
+                    <h1 className="category-title">Fashion</h1>
+                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
+                    <button className="category-btn">Shop Now</button>
+                </div>
+                <div className="bg-img">
+                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
+                </div>
+            </div>
+           <div className="category-card">
                 <div>
                     <h1 className="category-title">Fashion</h1>
                     <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
@@ -34,7 +62,19 @@ function CategoryList() {
                     <img src="../images/categories/Shoe.jpg" alt="background"></img>
                 </div>
             </div>
+           <div className="category-card">
+                <div>
+                    <h1 className="category-title">Fashion</h1>
+                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
+                    <button className="category-btn">Shop Now</button>
+                </div>
+                <div className="bg-img">
+                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
+                </div>
+            </div>
+        </Carousel>
         </div>
+       
     );
 }
 
