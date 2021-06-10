e024bdca29d96287bbb8023a883742aa2dca91d9
@reeeham <reeehamgamal@gmail.com>
2021-06-10 06:51:08 +0200

    categories component

--- src/containers/Home/components/category-list/index.js
+++ src/containers/Home/components/category-list/index.js
@@ -1,81 +1,51 @@
 import React from 'react'
 import "./index.scss"
 import Carousel from 'react-elastic-carousel';
-
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 const breakPoints = [
     { width: 1, itemsToShow: 3 }
-  ];
-  
+];
 
-function CategoryList() {
+
+function CategoryList(props) {
+    const { categories } = props;
     return (
         <div className="container">
-        <Carousel breakPoints={breakPoints} className="category-list">
- 
-            <div className="category-card">
-                <div>
-                    <h1 className="category-title">Fashion</h1>
-                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
-                    <button className="category-btn">Shop Now</button>
-                </div>
-                <div className="bg-img">
-                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
-                </div>
-            </div>
-            <div className="category-card">
-                <div>
-                    <h1 className="category-title">Fashion</h1>
-                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
-                    <button className="category-btn">Shop Now</button>
-                </div>
-                <div className="bg-img">
-                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
-                </div>
-            </div>
-           <div className="category-card">
-                <div>
-                    <h1 className="category-title">Fashion</h1>
-                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
-                    <button className="category-btn">Shop Now</button>
-                </div>
-                <div className="bg-img">
-                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
-                </div>
-            </div>
-            <div className="category-card">
-                <div>
-                    <h1 className="category-title">Fashion</h1>
-                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
-                    <button className="category-btn">Shop Now</button>
-                </div>
-                <div className="bg-img">
-                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
-                </div>
-            </div>
-            <div className="category-card">
-                <div>
-                    <h1 className="category-title">Fashion</h1>
-                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
-                    <button className="category-btn">Shop Now</button>
-                </div>
-                <div className="bg-img">
-                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
-                </div>
-            </div>
-           <div className="category-card">
-                <div>
-                    <h1 className="category-title">Fashion</h1>
-                    <p className="category-description">commodo dolor sit amet cum diceam est un partuient</p>
-                    <button className="category-btn">Shop Now</button>
-                </div>
-                <div className="bg-img">
-                    <img src="../images/categories/Shoe.jpg" alt="background"></img>
-                </div>
-            </div>
-        </Carousel>
+            <Carousel breakPoints={breakPoints} className="category-list">
+
+
+                {categories.map((cat,i) => {
+                    return(
+                        <div className="category-card" key={i}>
+                        <div className="category-content">
+                            <h1 className="category-title">{cat.title}</h1>
+                            <p className="category-description">{cat.description}</p>
+                            <button className="category-btn"><Link to={cat.action_link} >Shop Now</Link></button>
+                        </div>
+                        <div className="category-img">
+                            <img src={cat.image} alt={cat.alt_value}></img>
+                        </div>
+                    </div>
+                    )
+                 
+                })}
+                </Carousel>
         </div>
-       
+
     );
 }
 
 export default CategoryList
+
+
+CategoryList.propTypes = { 
+    categories : PropTypes.shape([{
+        id : PropTypes.number,
+        title: PropTypes.string,
+        description: PropTypes.string,
+        action_link: PropTypes.string,
+        image: PropTypes.string,
+        alt_value: PropTypes.string
+    }])
+}
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import CategoryList from './components/category-list';
-import HeroView from './components/HeroView'
-import "./index.scss"
+import HeroView from './components/HeroView';
+import "./index.scss";
+import { categoryList } from './services/categoryList';
 import { heroView } from './services/heroView';
 
 function Home() {
   const [heroViewData,setHeroViewData] = useState({});
+  const [categories,setCategories] = useState([]);
   useEffect(()=>{
     heroView().then(res => {
       
       setHeroViewData( res.data)
     }).catch((error) => {
       console.log('hero view error', error)
+    });
+
+    categoryList().then(res => {
+      setCategories(res.data);
+    }, (err) => {
+      console.log("error",err)
     })
 
   },[])
@@ -21,7 +29,7 @@ function Home() {
           <HeroView heroViewData={heroViewData}/>
         </div>
         <div className="main-container">
-          <CategoryList />
+          <CategoryList categories={categories} />
         </div>
         </>
     )
