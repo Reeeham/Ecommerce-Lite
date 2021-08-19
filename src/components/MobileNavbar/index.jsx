df8c21444bbfa09abfe1a5421994457bc2946aa8
rehaaam <reeehamgamal@gmail.com>
2021-08-20 00:02:57 +0200

    multi-level menu

--- src/components/MobileNavbar/index.jsx
+++ src/components/MobileNavbar/index.jsx
@@ -10,6 +10,9 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 
 const MobileNavbar = () => {
+    const handleCountryChange = (name) => { setSelectedCountryImgPath(name) }
+    const [selectedCountryImgPath, setSelectedCountryImgPath] = useState('./../../images/countries-icons/united-states.svg');
+
     return (
         <>
             <nav className="main-container second-header">
@@ -23,29 +26,93 @@ const MobileNavbar = () => {
                             </span>
 
                         </div>
-                      
+
                     </li>
-                    
+
                     <li>
 
                     </li>
 
                 </ul>
                 <ul className="nav-list">
-                <li>
-                    <button className="drop-btn" >
-                                All Products
-                                <FontAwesomeIcon
-                                    icon={faAngleDown}
-                                    style={{ marginLeft: "0.7rem" }}
-                                />
-                            </button>
-                            <ul class="sub-menu">
+                    <li>
+                        <button className="drop-btn" >
+                            All Products
+                            <FontAwesomeIcon
+                                icon={faAngleDown}
+                                style={{ marginLeft: "0.7rem" }}
+                            />
+                        </button>
+                        <ul class="sub-menu">
                             <li><Link to="">Fashion</Link></li>
                             <li><Link to="">Furniture</Link></li>
                             <li><Link to="">Electronics</Link></li>
                         </ul>
                     </li>
+                    <li>
+                        <button className="drop-btn">My Account
+                            <FontAwesomeIcon
+                                icon={faAngleDown}
+                                style={{ marginLeft: "0.7rem" }}
+                                className="cursor"
+                            />
+                        </button>
+                        <ul class="sub-menu">
+                            <li><Link to="">Profile</Link></li>
+                            <li><Link to="">Logout</Link></li>
+                            <li><Link to="">any</Link></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <button className="drop-btn">
+                            <img
+                                className="country-img cursor"
+                                alt="united states"
+                                src={selectedCountryImgPath}
+                            />
+                            <FontAwesomeIcon
+                                icon={faAngleDown}
+                                style={{ marginLeft: "0.5rem" }}
+                                className="cursor"
+                            />
+                        </button>
+                        <ul class="sub-menu">
+                            <li><Link to="" onClick={() => handleCountryChange('./../../images/countries-icons/egypt.svg')}>
+                                <img
+                                    className="country-img"
+                                    alt="united states"
+                                    src="./../../images/countries-icons/egypt.svg"
+                                /></Link>
+                            </li>
+                            <li>
+                                <Link to="" onClick={() => handleCountryChange('./../../images/countries-icons/united-kingdom.svg')}>
+                                    <img
+                                        className="country-img"
+                                        alt="united states"
+                                        src="./../../images/countries-icons/united-kingdom.svg"
+                                    /></Link>
+                            </li>
+                            <li>
+                                <Link to="" onClick={() => handleCountryChange('./../../images/countries-icons/united-states.svg')}>
+                                    <img
+                                        className="country-img"
+                                        alt="united states"
+                                        src="./../../images/countries-icons/united-states.svg"
+                                    /></Link>
+                            </li>
+                           
+                        </ul>
+                    </li>
+                    <li>
+                                <button className="drop-btn">
+                                    <FontAwesomeIcon
+                                        icon={faShoppingCart}
+                                        style={{ marginRight: "0.5rem" }}
+                                    /> Cart<span className="num">0</span>
+
+                                </button>
+                            </li>
+
                 </ul>
             </nav>
         </>
