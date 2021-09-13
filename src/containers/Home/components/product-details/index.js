5a352b4038fb9215d6e86f20698cdc117f768a8a
reeeham <reeehamgamal@gmail.com>
2021-09-13 08:51:43 +0200

    photo gallery in product details

--- src/containers/Home/components/product-details/index.js
+++ src/containers/Home/components/product-details/index.js
@@ -3,25 +3,40 @@ import './index.scss'
 
 export const ProductDetails = () => {
     return (<div className="container">
-
         <div className="product-details">
-            <div className="bread-crumbs">
-                <ul>
-                    <li>Home</li>
-                    <li>Shop</li>
-                    <li>Clothing</li>
-                    <li>Hoodies</li>
-                    <li>Happy Ninja</li>
-                </ul>
+            <div className="product-details-header">
+                <div className="bread-crumbs">
+                    <ul>
+                        <li>Home</li>
+                        <li>Shop</li>
+                        <li>Clothing</li>
+                        <li>Hoodies</li>
+                        <li>Happy Ninja</li>
+                    </ul>
+                </div>
             </div>
-            <div className="photo-gallery"></div>
-            <div className="title"></div>
-            <div className="reviews"></div>
-            <div className="price"></div>
-            <div className="description"></div>
-            <div className="color"></div>
-            <div className="add-to-cart"></div>
-            <div className="nav-tabs"></div>
+            <div className="product-details-body">
+                <div className="photo-gallery">
+                    <div className="first-img"><img src="../../../images/download.jpeg" alt="" /></div>
+                    <div className="other-images">
+                        <img src="../../../images/download.jpeg" alt="" />
+                        <img src="../../../images/download.jpeg" alt="" />
+                        <img src="../../../images/download.jpeg" alt="" />
+                        <img src="../../../images/download.jpeg" alt="" />
+
+                    </div>
+                </div>
+                <div className="title-and-desc">
+                    <div className="title"></div>
+                    <div className="reviews"></div>
+                    <div className="price"></div>
+                    <div className="description"></div>
+                    <div className="color"></div>
+                    <div className="add-to-cart"></div>
+                    <div className="nav-tabs"></div>
+                </div>
+
+            </div>
         </div>
 
     </div>)
