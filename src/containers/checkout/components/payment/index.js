ebef6fc55d07fba5b812f63857628090ea9369cc
reeeham <reeehamgamal@gmail.com>
2022-01-18 22:51:59 +0200

    payment form ui

--- src/containers/checkout/components/payment/index.js
+++ src/containers/checkout/components/payment/index.js
@@ -8,14 +8,28 @@ export const Payment = () => {
     return (<>
         <ul className="payment-options-list">
             <li className="payment-option-card">
+                <div className="payment-radio-button">
                     <input type="radio" className="payment-radio-button" />Online payment
-                    <div className="payment-icons">
-                        <FontAwesomeIcon icon={faCreditCard} color="blue" />
-                        <FontAwesomeIcon icon={faCreditCard} color="blue" />
-                    </div>
+                </div>
+                <div className="payment-icons">
+                    <FontAwesomeIcon icon={faCreditCard} color="blue" />
+                    <FontAwesomeIcon icon={faCreditCard} color="blue" />
+                    <FontAwesomeIcon icon={faCreditCard} color="blue" />
+                    <FontAwesomeIcon icon={faCreditCard} color="blue" />
+                </div>
             </li>
-            <li className="payment-option-card"><input type="radio" className="payment-radio-button" />Pay monthly by valU (above 500LE)</li>
-            <li className="payment-option-card"><input type="radio" className="payment-radio-button" />Cash on Delivery</li>
+            <li className="payment-option-card">
+                <div className="payment-radio-button">
+
+                    <input type="radio" className="payment-radio-button" />Pay monthly by valU (above 500LE)
+                </div>
+            </li>
+            <li className="payment-option-card">
+                <div className="payment-radio-button">
+
+                    <input type="radio" className="payment-radio-button" />Cash on Delivery
+                </div>
+            </li>
         </ul>
     </>)
 }
